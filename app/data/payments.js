3b763c3d9ccab987d3c9b84a5624609d6ba6c2a7
Drake <150128459+VialsShiny@users.noreply.github.com>
2026-01-15 15:30:45 +0100

    Adding Middleware && Getting User's Payements

---
+++ app/data/payments.js
@@ -0,0 +1,34 @@
+const payments = [
+    {
+        id: 1,
+        userId: 1,
+        price: 250.0,
+        date: '2026-01-10T14:30:00Z',
+    },
+    {
+        id: 2,
+        userId: 2,
+        price: 120.5,
+        date: '2026-01-11T09:15:00Z',
+    },
+    {
+        id: 3,
+        userId: 3,
+        price: 75.2,
+        date: '2026-01-12T18:45:00Z',
+    },
+    {
+        id: 4,
+        userId: 4,
+        price: 300.0,
+        date: '2026-01-13T12:00:00Z',
+    },
+    {
+        id: 5,
+        userId: 2,
+        price: 500.0,
+        date: '2026-01-14T15:30:00Z',
+    },
+];
+
+export default payments;
--- app/payments.js
+++ app/payments.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import payments from './data/payments.js';
 
 const SECRET = dotenv.config({path: './.env'}).parsed.SECRET;
 const PORT = 5000;
 const HOST = 'localhost';
 
 const app = express()
-    .post('/payments', async (req, res, next) => {
+    .use((req, res, next) => {
         const credentials = req.headers.authorization.split(' ');
 
         if (credentials.length !== 2 || credentials[0] !== 'Bearer') {
@@ -17,52 +18,41 @@ const app = express()
         const token = credentials[1];
         const payload = jwt.verify(token, SECRET);
 
+        req.payload = payload;
+        return next();
+    })
+    .get('/payments/:id', async (req, res, next) => {
+        const payload = req.payload;
+        if (!payload.scope.includes('payments:r')) {
+            return res.sendStatus(401);
+        }
+        console.log(payload);
+
+        const payment = payments.find(
+            (payment) => payment.userId == payload.id
+        );
+
+        if (!payment) payment = 'Pas de payement trouvé';
+
+        res.status(200).json({
+            status: 200,
+            message: 'Accés Autorisé',
+            payment,
+        });
+    })
+    .post('/payments', async (req, res, next) => {
+        const payload = req.payload;
         if (!payload.scope.includes('payments:rw')) {
             return res.sendStatus(401);
         }
 
         res.status(200).json({
             status: 200,
-            message: "Accés Autorisé"
+            message: 'Accés Autorisé',
         });
     })
-    .post('/invoices', async (req, res, next) => {
-        const credentials = req.headers.authorization.split(' ');
-
-        if (credentials.length !== 2 || credentials[0] !== 'Bearer') {
-            return res.sendStatus(401);
-        }
-
-        const token = credentials[1];
-        const payload = jwt.verify(token, SECRET);
-
-        if (!payload.scope.includes('payments:r')) {
-            return res.sendStatus(401);
-        }
-
-        res.status(200).json({
-            status: 200,
-            message: "Accés Autorisé"
-        });
-    })
-    .post('/debts', async (req, res, next) => {
-        const credentials = req.headers.authorization.split(' ');
-
-        if (credentials.length !== 2 || credentials[0] !== 'Bearer') {
-            return res.sendStatus(401);
-        }
-
-        const token = credentials[1];
-        const payload = jwt.verify(token, SECRET);
-
-        if (!payload.scope.includes('payments:r')) {
-            return res.sendStatus(401);
-        }
-
-        res.status(200).json({
-            status: 200,
-            message: "Accés Autorisé"
-        });
+    .delete('/payments/:id', async (req, res, next) => {
+        // delete payment
     });
 
 app.listen(PORT, HOST, () => {
